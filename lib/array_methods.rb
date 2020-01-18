abaa9fc9690e8e3aa91332799ae8e18a633d1dec
Christine Bumpous <bumpouce@gmail.com>
2020-01-18 01:47:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,7 +14,10 @@ def find_max_value(array)
   max_value = array.first
   array.length.times do |index|
     if max_value < array[index]
+      max_value = array[index]
+    end
   end
+  max_value
 end
 
 def find_min_value(array)
