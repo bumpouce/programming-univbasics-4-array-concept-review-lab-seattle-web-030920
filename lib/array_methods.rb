fcf2a2d72412f970290a3593bb4544d31e841c12
Christine Bumpous <bumpouce@gmail.com>
2020-01-18 01:44:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,11 +8,15 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  sorted_array = array.sort
-  sorted_array.last
+#  sorted_array = array.sort
+#  sorted_array.last
+
+  array.length.times do |index|
+  
+  end
 end
 
 def find_min_value(array)
-  sorted_array = array.sort
-  sorted_array.first
+#  sorted_array = array.sort
+#  sorted_array.first
 end
